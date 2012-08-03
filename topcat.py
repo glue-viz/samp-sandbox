9cfdf3bde47981deb39689b628b0f00e882ae4b0
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-08-03 16:44:27 +0200

    Added topcat example (select_rows doesn't work yet)

---
+++ topcat.py
@@ -0,0 +1,73 @@
+import os
+import urllib
+
+import atpy
+from sampy import SAMPHubProxy, SAMPClient
+
+import numpy as np
+
+
+class SAMPHelper(object):
+
+    def __init__(self):
+
+        self.myhub = SAMPHubProxy()
+        self.myhub.connect()
+
+        self.client = SAMPClient(self.myhub)
+
+        metadata1 = {"samp.name": "Client",
+                     "samp.description.text": "Client",
+                     "client.version": "0.01"}
+
+        self.client.start()
+        self.client.register()
+
+        self.client.declareMetadata(metadata1)
+
+    def send_table(self, filename):
+        '''
+        Send a table via SAMP
+
+        Parameters
+        ----------
+        filename : str
+            The table to send
+        '''
+
+        result = self.myhub.notifyAll(self.client.getPrivateKey(),
+                    {"samp.mtype": "table.load.votable",
+                     "samp.params": {"url": 'file://' + os.path.abspath(filename),
+                                     "name": filename}
+                    })
+
+        return result[0]
+
+    def select_rows(self, table_id, rows):
+
+        result = self.myhub.notifyAll(self.client.getPrivateKey(),
+                    {"samp.mtype": "table.select.rowList",
+                     "samp.params": {"table-id": table_id,
+                                     "row-list": rows}
+                    })
+
+    def finalize(self):
+
+        self.client.unregister()
+        self.client.stop()
+
+        self.myhub.disconnect()
+
+if __name__ == "__main__":
+
+    t = atpy.Table()
+    t.add_column('id', range(10))
+    t.add_column('flux', np.random.random(10))
+    t.write('test.xml', overwrite=True)
+
+    try:
+        h = SAMPHelper()
+        tid = h.send_table('test.xml')
+        h.select_rows(tid, [1, 3, 4])
+    finally:
+        h.finalize()
